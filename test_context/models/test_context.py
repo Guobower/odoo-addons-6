7ed72c18be538f3a27906400fb270dfd08a5fe71
jackjack <giacomo.grasso.82@gmail.com>
2018-07-25 09:13:17 +0200

    base of test context module

---
+++ test_context/models/test_context.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+#   @author Giacom Grasso <giacomo.grasso.82@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+from odoo.tools.safe_eval import safe_eval
+
+from datetime import datetime
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.multi
+    def action_confirm(self):
+        for order in self:
+            order.state = 'sale'
+            order.confirmation_date = fields.Datetime.now()
+            if self.env.context.get('send_email'):
+                self.force_quotation_send()
+            order = order.with_context(partner_id=order.partner_id)
+            order.order_line._action_procurement_create()
+        if self.env['ir.values'].get_default('sale.config.settings', 'auto_done_setting'):
+            self.action_done()
+        return True
